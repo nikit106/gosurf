0f238e2cee1e7e5df0fcd2d0163ab5707c6cf722
nikita <nikit0s96@mail.ru>
2019-10-23 16:34:42 +0300

    end amazing map

--- app/js/main.js
+++ app/js/main.js
@@ -5,10 +5,10 @@ $('.header__slider').slick({
     fade:true,
     prevArrow: '<img class = "slider-arrows slider-arrows__left" src = "img/arrows-left.svg" alt = ""></img>',
     nextArrow: '<img class = "slider-arrows slider-arrows__right" src = "img/arrows-right.svg" alt = ""></img>',
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-dotshead',
 });
 
-$('.slider-dots').slick({
+$('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
@@ -19,6 +19,16 @@ $('.serf-slider').slick({
     slidesToScroll: 1,
     prevArrow: '<img class = "slider-arrows slider-arrows__left" src = "img/arrows-left.svg" alt = ""></img>',
     nextArrow: '<img class = "slider-arrows slider-arrows__right" src = "img/arrows-right.svg" alt = ""></img>',
+    asNavFor: '.slider-map',
+});
+
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.serf-slider',
+    focusOnSelect: true,
 });
 
 

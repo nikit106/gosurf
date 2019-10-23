f8cfbae2a27b77e898166c0ec5e3831204c9343b
nikita <nikit0s96@mail.ru>
2019-10-23 14:50:51 +0300

    end slider

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,12 @@ $('.slider-dots').slick({
     asNavFor: '.header__slider',
 });
 
+$('.serf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class = "slider-arrows slider-arrows__left" src = "img/arrows-left.svg" alt = ""></img>',
+    nextArrow: '<img class = "slider-arrows slider-arrows__right" src = "img/arrows-right.svg" alt = ""></img>',
+});
+
 
 });

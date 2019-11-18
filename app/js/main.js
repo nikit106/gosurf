be45b33d38a14a39022d67438bd469f1923506a9
nikita <nikit0s96@mail.ru>
2019-11-18 21:17:46 +0300

    end html css no adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -31,7 +31,7 @@ $('.slider-map').slick({
     focusOnSelect: true,
 });
 
-$('.travel__slider').slick({
+$('.holder__slider, .shop__slider').slick({
     infinite: true,
     fade: true,
     prevArrow: '<img class = "slider-arrows slider-arrows__left" src = "img/arrows-left.svg" alt = ""></img>',
@@ -40,4 +40,50 @@ $('.travel__slider').slick({
 });
 
 
+
+
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src = "../img/Plus.svg" alt = ""></div><div class="quantity-button quantity-down"><img src = "../img/Minus.svg" alt = ""></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+        var spinner = jQuery(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity-up'),
+            btnDown = spinner.find('.quantity-down'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+    });
+
+    $('.quantity-button').on('click', function () {
+        let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+        $('.summ').html('$' + summ);
+    })
+
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+    $('.summ').html('$' + summ);
+
+    $('.surfboard-box__circle').on('click', function () {
+        $(this).toggleClass('active')
+    })
 });

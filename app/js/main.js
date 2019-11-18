b9a2e1a41b171cb98e80cd0c8e0e03aa7b49841a
nikita <nikit0s96@mail.ru>
2019-11-18 14:31:00 +0300

    end airline

--- app/js/main.js
+++ app/js/main.js
@@ -31,5 +31,13 @@ $('.slider-map').slick({
     focusOnSelect: true,
 });
 
+$('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class = "slider-arrows slider-arrows__left" src = "img/arrows-left.svg" alt = ""></img>',
+    nextArrow: '<img class = "slider-arrows slider-arrows__right" src = "img/arrows-right.svg" alt = ""></img>',
+
+});
+
 
 });
